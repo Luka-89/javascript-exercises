008ede5cf6602cc8ff079d565a4cdf4f60b4bfa6
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-22 19:25:48 +0200

    Finish sumAll;

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if(!Number.isInteger(n1) || !Number.isInteger(n2) || n1 < 0 || n2 < 0) return 'ERROR';
+    if(n1 > n2) {
+        let a = n1;
+        n1 = n2;
+        n2 = a;
+    }
+    return (n2 - n1 + 1)/2*(n1 + n2);
 };
 
 // Do not edit below this line
