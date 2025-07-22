82091e250401d99ca70aa846297f3dca6a60dbf2
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-22 19:14:44 +0200

    Finish removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    let res = [];
+    arr.forEach(item => {
+        if(!arg.includes(item)) res.push(item);
+    })
+    return res;
 };
 
 // Do not edit below this line
