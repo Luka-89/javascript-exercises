8d48c192e09364eff442e4c952297501bd1192df
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-23 19:13:05 +0200

    Finish 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let C = (F - 32)*(5/9);
+  return parseFloat(C.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let F = C * (9/5) + 32;
+  return parseFloat(F.toFixed(1));
 };
 
 // Do not edit below this line
