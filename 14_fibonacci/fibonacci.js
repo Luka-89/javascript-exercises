a165f7556eb86a39de89a72363270d6174cf4b68
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-29 13:28:46 +0200

    Finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = parseInt(a);
+    if(isNaN(a) || a < 0) return "OOPS";
+    if(a === 0) return 0;
+    let prev = 1;
+    let curr = 1;
 
+    for(let i = 0; i < a - 2; i++) {
+        curr = curr + prev;
+        prev = curr - prev;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
