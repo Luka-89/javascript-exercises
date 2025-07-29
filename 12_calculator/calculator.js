f004233827dd4061d8a650b70e28248df23a4941
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-29 12:46:58 +0200

    Finish 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum = 0;
+  for(let num of a) sum += num;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let product = 1;
+  for(let num of a) product *= num;
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for(let i = 1; i <= a; i++) factorial *= i;
+  return factorial;
 };
 
 // Do not edit below this line
