2de5b974f16a50f9df3b8be7fb470cfef4f92ced
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-29 13:15:35 +0200

    Finish 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '');
+    str = str.toLowerCase();
 
+    let middleRight = Math.floor(str.length/2);
+    let middleLeft;
+    if(str.length % 2 === 0) middleLeft = middleRight - 1;
+    else middleLeft = middleRight;
+
+    for(let i = 0; i <= middleLeft; i++) {
+        if(str.charAt(middleRight + i) !== str.charAt(middleLeft - i)) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
