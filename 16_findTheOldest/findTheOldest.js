b81c571851a964c8f59de8930fc37b0969548f73
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-29 13:56:16 +0200

    Finish javascript-exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let res = {
+        name: "Not a person",
+        yearOfBirth: 0,
+        yearOfDeath: 0,
+    }
 
+    return people.reduce((res, person) => {
+        
+        let resAge = res.yearOfDeath - res.yearOfBirth;
+        let personAge = person.yearOfDeath - person.yearOfBirth;
+
+        if(res.yearOfDeath === undefined) resAge = 2025 - res.yearOfBirth;
+        else if(person.yearOfDeath === undefined) personAge = 2025 - person.yearOfBirth;
+        
+        if (resAge <= personAge) return person;
+        else return res;
+    }, res);
 };
 
 // Do not edit below this line
